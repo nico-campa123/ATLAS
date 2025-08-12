f7ad635cd845656fef5681c79a59e6a6fe90543d
nico-campa123 <48675751@est.ort.edu.ar>
2025-08-11 21:11:12 -0300

    comienzo javascript

---
+++ BFS.js
@@ -0,0 +1,20 @@
+//pasos
+//MAPA = una array 1d 
+let rows = 100 //cambia
+let columns = 100
+let total_nodos = rows * columns
+let aulas = [19477,19466,18565,18554,11421,
+    11410,10509,10498,1887,1049,
+    138, 1061, 149, 1871, 1835] //checkear con tamaño actualizado
+let orientaciones = ['I', 'D', 'I', 'D', 'I', 'D', 'I', 'D', 'R', 'A', 'R', 'A', 'R', 'R', 'R']
+
+//ADJACENCY MATRIX: array 2d, cada nodo tiene [0,1,0,1] para sus cuatro vecinos
+//cacho complicado para completar el adj matric
+
+//def bfs(s,e / start,end)
+//  def solve
+
+let salida = aula[0]
+let llegada = aula[1]
+
+// choclo = bfs(salida,llegada)
